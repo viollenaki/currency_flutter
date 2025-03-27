37872b7d44e5b78264d2219a41d9aaac05825188
yntymakabc <yntymak.almazbekuulu1@gmail.com>
2025-03-27 17:31:05 +0600

    Update main.dart

--- my_flutter_app/lib/main.dart
+++ my_flutter_app/lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Currency Flutter Apppp',
+      title: 'Currency Flutter App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
